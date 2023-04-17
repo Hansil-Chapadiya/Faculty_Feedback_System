1a4159202140a86cc0ad93bdac427813f9049691
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2023-04-17 15:50:42 +0530

    Log_out add

---
+++ Project_UI2/Project_UI/logout.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_UI
+{
+    public partial class logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+            Response.Redirect("./Login_new.aspx");
+        }
+    }
+}
